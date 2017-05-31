97f205ecb80c17317a61a93d4c53c52c987123d3
Samu-PC\Samu Aronen <samu.aronen@cs.tamk.fi>
2017-06-01 00:37:04 +0300

    Added some particle ssystem bases, way to change scale of the orb, and an object rotator

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    protected enum RotationAxis
+    {
+        X,
+        Y,
+        Z
+    }
+
+    [SerializeField] protected RotationAxis RotationDirection;
+    [SerializeField] protected float RotationSpeed = 10f;
+    protected Transform _transform;
+
+    void Start()
+    {
+        _transform = transform;
+    }
+
+    protected virtual void Update()
+    {
+        switch (RotationDirection)
+        {
+            case RotationAxis.X:
+                _transform.Rotate(new Vector3(Time.deltaTime * RotationSpeed, 0, 0));
+                break;
+            case RotationAxis.Y:
+                _transform.Rotate(new Vector3(0, Time.deltaTime * RotationSpeed, 0));
+
+                break;
+            case RotationAxis.Z:
+                _transform.Rotate(new Vector3(0, 0, Time.deltaTime * RotationSpeed));
+
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/ScaleLerper.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleLerper : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve _lerpCurve;
+
+    [SerializeField] private float _lerpTime;
+
+
+   [SerializeField] private float _maxScale;
+
+   [SerializeField] private float _minScale;
+
+    private float _lerpTimer;
+
+    private bool _forward = true;
+
+    private Transform _transform;
+
+    private float _currentScaleValue;
+
+    // Use this for initialization
+    void Start()
+    {
+        _transform = transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lerpStep = Mathf.Clamp01(_lerpCurve.Evaluate(
+               1 - (_lerpTimer / _lerpTime)));
+
+        _lerpTimer -= Time.deltaTime;
+
+
+        if (_forward)
+        {
+           
+
+
+            _currentScaleValue = Mathf.Lerp(_minScale, _maxScale, lerpStep);
+
+            _transform.localScale = new Vector3(_currentScaleValue, _currentScaleValue, _currentScaleValue);
+
+            if (_lerpTimer <= 0)
+            {
+                _forward = false;
+                _lerpTimer = _lerpTime;
+            }
+
+        }
+        else
+        {
+
+            _currentScaleValue = Mathf.Lerp(_maxScale, _minScale, lerpStep);
+
+            _transform.localScale = new Vector3(_currentScaleValue, _currentScaleValue, _currentScaleValue);
+
+
+            if (_lerpTimer <= 0)
+            {
+                _forward = true;
+                _lerpTimer = _lerpTime;
+            }
+
+        }
+
+    }
+}
