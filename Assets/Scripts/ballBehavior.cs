e6503b83739ff3973f11eac20bfd678479393451
jkgman <jgranger@gmail.com>
2017-05-31 12:53:06 +0300

    Balloon Showing and Hiding

---
+++ Assets/Scripts/ballBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballBehavior : MonoBehaviour {
+	private float _timer = 2;
+	private MeshRenderer _renderer;
+	// Use this for initialization
+	void Start () {
+		int count;
+		_renderer = GetComponent<MeshRenderer> ();
+		_renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_timer -= Time.deltaTime;
+
+		if(_timer < 0){
+			_renderer.enabled = true;
+		}
+
+	}
+}
