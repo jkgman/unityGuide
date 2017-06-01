75c1aea83e5aad8262287aaf81655d37d68aff9d
Samu-PC\Samu Aronen <samu.aronen@cs.tamk.fi>
2017-06-01 15:34:38 +0300

    Music script

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioClip _music;
+    [SerializeField] private AudioSource _musicSource;
+
+    void Start()
+    {
+        _musicSource = GetComponent<AudioSource>();
+        PlayMusic();
+    }
+
+
+    public void PlayMusic()
+    {
+        _musicSource.clip = _music;
+        _musicSource.Play();
+    }
+}
